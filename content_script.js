75b5ffee0fbb56cfbae43850efddbf790e835676
Jeffrey Miller <jeffmax@gmail.com>
2012-12-15 18:26:33 -0500

    Get meta tag data and bookmark tree.

--- content_script.js
+++ content_script.js
@@ -1,3 +1,17 @@
-console.log("sending message to extension");
-chrome.extension.sendMessage({title: document.title}, function(response) {
+var metaTags = document.getElementsByTagName("meta");
+var response = {};
+
+for (var metaKey in metaTags){
+    var metaTag = metaTags[metaKey];
+    if (metaTag.name && metaTag.name.toLowerCase() == "description"){
+        response.description = metaTag.content.toLowerCase().split(" ");
+    }
+    if (metaTag.name && metaTag.name.toLowerCase() == "keywords"){
+        response.keywords = metaTag.content.toLowerCase().split(",");
+        for (keywordKey in response.keywords)
+            response.keywords[keywordKey] = response.keywords[keywordKey].trim()
+    }
+}
+
+chrome.extension.sendMessage(response, function(response) {
 });
--- eventPage.js
+++ eventPage.js
@@ -1,4 +1,44 @@
 chrome.extension.onMessage.addListener(
     function(request, sender, sendResponse) {
-       console.log(request.title);
+       console.log(request);
+       console.log(sender);
+       getOtherBookmarksChildren(function(other){
+           if (other == null) {
+               console.error("'Other Bookmarks' not found."); 
+               return;
+           }
+           var folders = retrieveFolders(other);
+           var folder = determineBestFolder(request, folders);
+       });
 });
+
+function getOtherBookmarksChildren(callback) {
+    chrome.bookmarks.getTree(function(results){
+        var title = "Other Bookmarks";
+        var topLevel = results[0].children;
+        for (var nodeKey in topLevel){
+           var node = topLevel[nodeKey];
+           if (node.title == title)
+              return callback(node.children);
+        }
+        return callback(null);
+     });
+}
+
+function retrieveFolders(folder) {
+    var folders = [];
+    for (var nodeKey in folder){
+      var node = folder[nodeKey];
+      if (!node.hasOwnProperty("url"))
+          folders.push(node);
+    }
+    return folders;
+}
+
+function getUncategorizedFolder(folders){
+
+}
+
+function determineBestFolder(page, folders){
+  return "Folder";
+}
