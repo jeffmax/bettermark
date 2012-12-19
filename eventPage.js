3c57ad783f918f1763b8e7cc0e163753b84ce3f5
Jeffrey Miller <jeffmax@gmail.com>
2012-12-18 22:31:44 -0500

    All functions implemented, not fully tested.

--- eventPage.js
+++ eventPage.js
@@ -7,19 +7,19 @@ chrome.extension.onMessage.addListener(
           }
           var folders = retrieveFolders(other, otherID);
           var alreadyBookmarked = false;
-          var bookmarkID = null;
+          var bookmark = null;
           var bookmarkFolderID = null;
           chrome.bookmarks.search(sender.tab.url, function(results){
-              results.forEach(function(bookmark){
-                 if (bookmark.url == sender.tab.url){
+              results.forEach(function(currentBookmark){
+                 if (currentBookmark.url == sender.tab.url){
                    alreadyBookmarked = true; 
-                   bookmarkFolderID = bookmark.parentId;
-                   bookmarkID = bookmark.id;
+                   bookmarkFolderID = currentBookmark.parentId;
+                   bookmark = currentBookmark;
                    return false;
                  }
               });
               if (alreadyBookmarked){
-                     populateInterface(bookmarkFolderID, folders, sender.tab, bookmarkID);
+                     populateInterface(bookmarkFolderID, folders, bookmark);
                   return;
               }
               var folder = determineBestFolder(sender.tab, request, folders);
@@ -29,14 +29,14 @@ chrome.extension.onMessage.addListener(
                       chrome.bookmarks.create({'parentId': uncategorized.id,
                             'title': sender.tab.title || "No title",
                             'url': sender.tab.url}, function(bookmark) {
-                         populateInterface(uncategorized.id, folders, sender.tab, bookmark.id);
+                         populateInterface(uncategorized.id, folders, bookmark);
                       });
                   });
               }else{
                   chrome.bookmarks.create({'parentId': folder.id,
                      'title': sender.tab.title,
                      'url': sender.tab.url}, function(bookmark) {
-                     populateInterface(folder.id, folders, sender.tab, bookmark.id);
+                     populateInterface(folder.id, folders, bookmark);
                   });
               }
               // At this point folder will be the folder containing the bookmark
@@ -81,28 +81,54 @@ function getUncategorizedFolder(folders, otherID, callback){
     );
 }
 
-function populateInterface(folderID, otherFolders, page, bookmarkID){
+function populateInterface(folderID, otherFolders, bookmark){
     var views = chrome.extension.getViews({type:"popup"});
     var pop_doc = views[0].document;
     var title_input = pop_doc.getElementById("bookmark_title");
     var folder_input =  pop_doc.getElementById("bookmark_folders");
-    title_input.value = page.title;
+    title_input.value = bookmark.title;
+    
+    title_input.addEventListener("keyup", function(event) {
+        chrome.bookmarks.update(bookmark.id, {title: title_input.value}, function(){});
+    });
+    
     chrome.bookmarks.getRecent(1, function(bookmarks){
         if (bookmarks.length > 0)
             id = bookmarks[0].parentId
         else
             id = null
+        var cache_option;
         otherFolders.forEach(function(currentFolder){
             var newOption = new Option(currentFolder.title, currentFolder.id);
             newOption.selected = (folderID == currentFolder.id);
-            if (currentFolder.id == id) newOption.className = "last";
+            if (currentFolder.id == id) {
+                newOption.className = "last";
+                cache_option = newOption;
+            }
             folder_input.add(newOption, null);
         });
         pop_doc.getElementById("remove").addEventListener("click", function(){
-            chrome.bookmarks.remove(bookmarkID);
+            chrome.bookmarks.remove(bookmark.id);
             window.close();
         }, false);
+        // Setup move to last used folder option 
+        var last_folder = pop_doc.getElementById("last_folder");
+        last_folder.addEventListener("click", function(){
+            if (cache_option != null){
+               chrome.bookmarks.move(bookmark.id, {parentId:id},function(){
+                    cache_option.selected = true; 
+               });
+            }else{
+               //TODO notify user of error somehow 
+            }
+        });
+        // Setup change handler
+        folder_input.addEventListener("change", function(event) {
+           chrome.bookmarks.move(bookmark.id, {parentId: folder_input.selectedOptions[0].value}, function(){});
+        });
     });
+
+
 }
 
 // For now just look for folder name in page title
